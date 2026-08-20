8b88738ee0569ff4d4cb69610d47e8396f916635
ankithhegde-17 <ankithhegde001@gmail.com>
2026-08-20 07:04:24 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (78.87%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int jump(std::vector<int>& nums) {
+        int jumps = 0, current_end = 0, farthest = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            farthest = std::max(farthest, i + nums[i]);
+            if (i == current_end) {
+                jumps++;
+                current_end = farthest;
+            }
+        }
+        return jumps;
+    }
+};
