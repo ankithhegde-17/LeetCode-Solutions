ffd7d335c4f4d6008f46f54db94e19a693080278
Ankith Hegde <ankithhegde001@gmail.com>
2026-09-23 07:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (58.48%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+class Solution {
+public:
+    int singleNumber(std::vector<int>& nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+};
